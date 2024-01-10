b15cfb7bfc55be038787b0a02982bde82657b4a3
alice <126924511+wh4l35h4rk@users.noreply.github.com>
2024-01-10 23:55:20 +1000

    feat(drf): authorization

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -9,11 +9,12 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'userdata', views.UserDataViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('login/', views.UserLoginView.as_view()),
+    path('logout/', views.UserLogout)
 ]
 
 urlpatterns += router.urls
--- djangoproject/mainapp/views.py
+++ djangoproject/mainapp/views.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 import mainapp.serializers as my_seriazliers
 import mainapp.models as my_models
@@ -21,3 +27,23 @@ class UserDataViewSet(viewsets.ModelViewSet):
     queryset = my_models.UserData.objects.all()
     serializer_class = my_seriazliers.UserDataSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data['username'], 
+            password=request.data['password']
+        )
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+            return Response(data={'token': token.key})
+        else:
+            return Response(data={'error': 'invalid data >:('}, status=401)
+
+
+def UserLogout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
