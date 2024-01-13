12801dd27dd66fdf0217be025f67b00ddb5f157a
Derlie Monne <derlie.monne@gmail.com>
2024-01-13 17:49:13 +1000

    feat(model): gender [-1, 1] validation in model

--- djangoproject/mainapp/models.py
+++ djangoproject/mainapp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
@@ -12,5 +13,5 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
 class UserData(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     date_of_birth = models.DateField()
-    gender = models.FloatField()
+    gender = models.FloatField(validators=[MinValueValidator(-1), MaxValueValidator(1)])
     favorite_color = models.CharField(max_length=7)
